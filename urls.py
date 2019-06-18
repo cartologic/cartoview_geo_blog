374d04b5f5111816d4f5d819febea1d04c606d72
Khaled Boka <bokae97@gmail.com>
2019-06-18 17:47:27 +0200

    django 1.11 compatible

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import  url, include
 from django.views.generic import TemplateView
 from . import views, APP_NAME
-from cartoview.urls import urlpatterns as carto_urls
 
-urlpatterns = patterns('',
+urlpatterns = [
                        url(r'^$', views.index, name='%s.list' % APP_NAME),
                        url(r'^test$', views.test, name='%s.test' % APP_NAME),
                        url(r'^new/$', views.new, name='%s.new' % APP_NAME),
-                       url(r'^(?P<id>[\d]+)/edit/$', views.edit, name='%s.edit' % APP_NAME),
-                       url(r'^(?P<id>[\d]+)/view/$', views.details, name='post-details'),
-
-                       )
+                       url(r'^(?P<id>[\d]+)/edit/$', views.edit,
+                           name='%s.edit' % APP_NAME),
+                       url(r'^(?P<id>[\d]+)/view/$',
+                           views.details, name='post-details'),
+]
